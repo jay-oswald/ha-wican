62eb5cd8637dafae7c5c7e3563a4ab13f0b19d48
jwiesel <jochen.wiesel@posteo.de>
2025-02-16 12:13:09 +0100

    Added translation strings for error messages during config flow.

--- custom_components/wican/config_flow.py
+++ custom_components/wican/config_flow.py
@@ -6,14 +6,18 @@ import voluptuous as vol
 from .const import DOMAIN
 from .wican import WiCan
 
-DATA_SCHEMA = vol.Schema({
-    vol.Required(CONF_IP_ADDRESS): str,
-    })
+DATA_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_IP_ADDRESS): str,
+    }
+)
 _LOGGER = logging.getLogger(__name__)
+
 
 class WiCanConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
+
     async def async_step_user(self, user_input: dict[str, Any] | None = None):
         errors = {}
         if user_input is not None:
@@ -25,15 +29,14 @@ class WiCanConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 if info:
                     return self.async_create_entry(title="WiCAN", data=user_input)
                 else:
-                    errors[CONF_IP_ADDRESS] = "Failed validation, double check the IP, as well as check if you have protocol set to auto_pid"
+                    errors["base"] = "invalid_config"
             except ConnectionError:
                 _LOGGER.exception("Connection Error")
-                errors[CONF_IP_ADDRESS] = "WiCAN Connection error, are you sure the IP is correct?"
+                errors["base"] = "cannot_connect"
             except Exception:
                 _LOGGER.exception("Unexpected exception")
-                errors[CONF_IP_ADDRESS] = "WiCAN not validated, unknown error"
-                
+                errors["base"] = "unknown"
+
         return self.async_show_form(
             step_id="user", data_schema=DATA_SCHEMA, errors=errors
         )
-
